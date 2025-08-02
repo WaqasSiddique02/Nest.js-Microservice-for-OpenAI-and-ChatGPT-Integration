ab5f6815a95f915b1d72cf1c663e1175350443ca
Muhammad Waqas Siddique <siddique02mwaqas@gmail.com>
2025-08-02 19:35:15 +0500

    integrated openai and added simple prompt and response function

--- src/openai/openai.module.ts
+++ src/openai/openai.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { OpenaiService } from './openai.service';
+import { SupabaseService } from 'src/supabase/supabase.service';
+import OpenAI from 'openai';
 
 @Module({
-  providers: [OpenaiService],
+  providers: [
+    OpenaiService,
+    SupabaseService,
+    {
+      provide: OpenAI,
+      useFactory: () => {
+        return new OpenAI({
+          apiKey: process.env.OPEN_AI_SECRET_KEY,
+        });
+      },
+    },
+  ],
+  exports: [OpenaiService],
 })
 export class OpenaiModule {}
--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ServiceUnavailableException } from '@nestjs/common';
+import { SupabaseService } from 'src/supabase/supabase.service';
+import OpenAIApi from 'openai';
+import { ChatCompletion, ChatCompletionMessageParam } from 'openai/resources';
 
 type Message = {
   text: string;
@@ -7,7 +10,48 @@ type Message = {
 
 @Injectable()
 export class OpenaiService {
-    constructor(){
-        
+  constructor(
+    private readonly supabaseService: SupabaseService,
+    private readonly openai: OpenAIApi,
+  ) {
+    this.openai = new OpenAIApi({
+      apiKey: process.env.OPEN_AI_SECRET_KEY,
+    });
+  }
+
+  async chatGptResponse(prompt: string, messages: Message[]): Promise<string> {
+    try {
+      const history = messages.map(
+        (message): ChatCompletionMessageParam => ({
+          role: message.ai ? 'assistant' : 'user',
+          content: message.text,
+        }),
+      );
+
+      // Make a request to the ChatGPT model
+      const completion: ChatCompletion =
+        await this.openai.chat.completions.create({
+          model: 'gpt-4',
+          messages: [
+            {
+              role: 'system',
+              content: prompt,
+            },
+            ...history,
+          ],
+          temperature: 0.5,
+          max_tokens: 1000,
+        });
+
+      const [content] = completion.choices.map(
+        (choice) => choice.message.content ?? '',
+      );
+
+      return content;
+    } catch (e) {
+      // Log and propagate the error
+      console.error(e);
+      throw new ServiceUnavailableException('Failed request to ChatGPT');
     }
+  }
 }
